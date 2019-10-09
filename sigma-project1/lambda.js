4e9e508ea065ce300aa6acad65f92f360c5752a3
salmanprk <salman.alam@alumni.ubc.ca>
2019-10-10 04:05:24 +0600

    First Commit

---
+++ sigma-project1/lambda.js
@@ -0,0 +1,33 @@
+let AWS = require('aws-sdk');
+const ses = new AWS.SES();
+
+exports.handler = function (event, context, callback) {
+
+    let email = event['email']
+    let subject = event['subject']
+    let body = event['body']
+    ses.sendEmail({
+        Destination: {
+            ToAddresses: [email],
+            CcAddresses: [],
+            BccAddresses: []
+        },
+        Message: {
+            Body: {
+                Text: {
+                    Data: body
+                }
+            },
+            Subject: {
+                Data: subject
+            }
+        },
+        Source: 'salman@fostrum.com'
+    }, function (err, data) {
+        if (err) console.log(err, err.stack); // an error occurred
+        else console.log(data);           // successful response
+    });
+
+
+    callback(null, { "message": "Successfully executed" });
+}
