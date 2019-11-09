4998dfa7d8bf3c7957cc9a56a0d780ef9217de5d
salmanprk <salman.alam@alumni.ubc.ca>
2019-11-09 07:24:23 +0600

    Updated API Gateway

--- sigma-project1/lambda.js
+++ sigma-project1/lambda.js
@@ -22,7 +22,7 @@ exports.handler = function (event, context, callback) {
                 Data: subject
             }
         },
-        Source: 'salman@fostrum.com'
+        Source: 'info@fostrum.com'
     }, function (err, data) {
         if (err) console.log(err, err.stack); // an error occurred
         else console.log(data);           // successful response
